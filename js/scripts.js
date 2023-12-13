b2f7d88d25f7bdfdce84ca3eaf12fec6f54873f2
rpark199 <147426615+rpark199@users.noreply.github.com>
2023-12-13 14:48:53 -0500

    Update arrays scripts.js
    Updated arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-
+//const pokemonList= [
+// {
+//   id: 1,
+//   name: "Charizard",
+//   height: 1.7,
+//   weight: 90.5,
+//   type: ["fire"],
+// },
+// {
+//   id: 2,
+//   name: "Venusaur",
+//   height: 2,
+//   weight: 100,
+//   type: ["grass"],
+// },
+// {
+//   id: 3,
+//   name: "Dragonite",
+//   height: 2.2,
+//   weight: 210,
+//   type: ["water"],
+// },
+// ];
+const pokemonList= ["Charizard", "Venusaur", "Dragonite"];
+let pokemonList= pokemonList[0];
+let pokemonList=[
+    1,
+    [1.7, 90.5],
+    {type: 'fire'}
+];
+let pokemonList= pokemonList[1];
+let pokemonList=[
+    2,
+    [2, 100],
+    {type: 'grass'}
+];
+let pokemonList=pokemonList[2];
+let pokemonList=[
+    3,
+    [2.2, 210],
+    {type: 'water'}
+];
