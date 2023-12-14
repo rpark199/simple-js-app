c9da599ef3dc17b6fdfeaa62e434314758b86560
rpark199 <147426615+rpark199@users.noreply.github.com>
2023-12-14 10:33:30 -0500

    Deleted comments and corrected errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,23 @@
-//const pokemonList= [
-// {
-//   id: 1,
-//   name: "Charizard",
-//   height: 1.7,
-//   weight: 90.5,
-//   type: ["fire"],
-// },
-// {
-//   id: 2,
-//   name: "Venusaur",
-//   height: 2,
-//   weight: 100,
-//   type: ["grass"],
-// },
-// {
-//   id: 3,
-//   name: "Dragonite",
-//   height: 2.2,
-//   weight: 210,
-//   type: ["water"],
-// },
-// ];
-const pokemonList= ["Charizard", "Venusaur", "Dragonite"];
-let pokemonList= pokemonList[0];
-let pokemonList=[
-    1,
-    [1.7, 90.5],
-    {type: 'fire'}
-];
-let pokemonList= pokemonList[1];
-let pokemonList=[
-    2,
-    [2, 100],
-    {type: 'grass'}
-];
-let pokemonList=pokemonList[2];
-let pokemonList=[
-    3,
-    [2.2, 210],
-    {type: 'water'}
+let pokemonList = [
+    {
+        id: 1,
+        name: "Charizard",
+        height: 1.7,
+        weight: 90.5,
+        types: ["fire"],
+    },
+    {
+        id: 2,
+        name: "Venusaur",
+        height: 2,
+        weight: 100,
+        types: ["grass"],
+    },
+    {
+        id: 3,
+        name: "Dragonite",
+        height: 2.2,
+        weight: 210,
+        types: ["water"],
+    },
 ];
