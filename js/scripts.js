0845e3be4570048b98771e1871e4b7fdc1eefd62
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-01-11 19:02:53 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,5 @@
 let pokemonRepository= (function(){
-    let pokemonList = [
-        {
-            id: 1,
-            name: "Charizard",
-            height: 1.7,
-            weight: 90.5,
-            types: ["fire"],
-        },
-        {
-            id: 2,
-            name: "Venusaur",
-            height: 2,
-            weight: 100,
-            types: ["grass"],
-        },
-        {
-            id: 3,
-            name: "Dragonite",
-            height: 2.2,
-            weight: 210,
-            types: ["water"],
-        },
-    ];
+    let pokemonList = []
     let apiUrl= "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
     for (let i=0; i < pokemonList.length; i++) {
